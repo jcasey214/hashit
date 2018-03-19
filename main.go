b0e6ab2e3106aee242ed6780cafcb5fdff868f3f
Jonathan Casey <jon_casey@me.com>
2018-03-18 23:18:08 -0600

    shutdown gracefully on sigint or /shutdown

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"github.com/jcasey214/hashit/handler"
 	"log"
+	"time"
+	"os/signal"
+	"context"
 )
 
 func main() {
@@ -15,8 +18,31 @@ func main() {
 	} else {
 		port = "8080"
 	}
-	fmt.Printf("listening on port %s \n", port)
 
-	http.HandleFunc("/hash", handler.HashHandler)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	stopChan := make(chan os.Signal)
+	signal.Notify(stopChan, os.Interrupt)
+
+	mux := http.NewServeMux()
+	srv := &http.Server{Addr: fmt.Sprintf(":%s", port), Handler: mux}
+	log.Printf("listening on port %s \n", port)
+
+	mux.Handle("/hash", http.HandlerFunc(handler.HashHandler))
+	mux.Handle("/shutdown", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		srv.Shutdown(ctx)
+	}))
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+
+	<-stopChan
+	log.Println("Shutting down server...")
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	srv.Shutdown(ctx)
+
+	log.Println("Server gracefully stopped")
 }
