4aacf428511064b513633d1dbf5c7f41a111145a
Jonathan Casey <jon_casey@me.com>
2018-03-19 20:52:30 -0600

    return hash identifier, store hash in memory

--- handler/handler.go
+++ handler/handler.go
@@ -5,22 +5,33 @@ import (
 	"fmt"
 	"time"
 	"github.com/jcasey214/hashit/hash"
+	"strconv"
 )
+
+var hashes []string
 
 func HashHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r)
 	if r.Method == "POST" {
+		r.ParseForm()
 		pw := r.FormValue("password")
 
 		if pw == "" {
 			http.Error(w, "Missing required field in request", 400)
 		} else {
-			time.Sleep(5 * time.Second)
+			hashes = append(hashes, "")
+			newIndex := len(hashes) - 1
 			w.Header().Set("Content-Type", "text/plain")
-			r.ParseForm()
-			w.Write([]byte(hash.Hash(pw)))
+			go hashPassword(pw, newIndex)
+			w.Write([]byte(strconv.Itoa(newIndex)))
 		}
 	} else {
 		http.Error(w, "Invalid request method.", 405)
 	}
+}
+
+func hashPassword(password string, index int) {
+	time.Sleep(5*time.Second)
+	hashes[index] = hash.Hash(password)
+	fmt.Println(hashes)
 }
