8c6052c2c1438413066809786298e6a90e1646ec
Jonathan Casey <jon_casey@me.com>
2018-03-20 12:48:51 -0600

    add mutex to avoid race conditions in hash store

--- handler/handler.go
+++ handler/handler.go
@@ -6,9 +6,15 @@ import (
 	"github.com/jcasey214/hashit/hash"
 	"strconv"
 	"strings"
+	"sync"
 )
 
-var hashes []string
+type HashStore struct {
+	hashes []string
+	mutex  sync.Mutex
+}
+
+var h = HashStore{hashes: []string{}}
 
 func CreateHash(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
@@ -18,8 +24,10 @@ func CreateHash(w http.ResponseWriter, r *http.Request) {
 		if pw == "" {
 			http.Error(w, "Missing required field in request", 400)
 		} else {
-			hashes = append(hashes, "")
-			newIndex := len(hashes) - 1
+			h.mutex.Lock()
+			h.hashes = append(h.hashes, "")
+			newIndex := len(h.hashes) - 1
+			h.mutex.Unlock()
 			w.Header().Set("Content-Type", "text/plain")
 			go hashAndSave(pw, newIndex)
 			w.Write([]byte(strconv.Itoa(newIndex)))
@@ -36,10 +44,10 @@ func GetHashById(w http.ResponseWriter, r *http.Request) {
 
 		if err != nil {
 			http.Error(w, "Bad Request", 400)
-		} else if index >= len(hashes) {
+		} else if index >= len(h.hashes) {
 			http.Error(w, "Not Found", 404)
 		} else {
-			w.Write([]byte(hashes[index]))
+			w.Write([]byte(h.hashes[index]))
 		}
 
 	} else {
@@ -49,5 +57,5 @@ func GetHashById(w http.ResponseWriter, r *http.Request) {
 
 func hashAndSave(password string, index int) {
 	time.Sleep(5 * time.Second)
-	hashes[index] = hash.Hash(password)
+	h.hashes[index] = hash.Hash(password)
 }
